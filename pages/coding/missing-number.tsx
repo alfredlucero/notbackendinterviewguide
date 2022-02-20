e50cb2685469dba873d9851439c13a9e6aa6f77e
Alfred Lucero <alfred.lucero@ucla.edu>
2022-02-20 09:19:34 -0800

    Added XOR approach for missing numbers

--- pages/coding/missing-number.tsx
+++ pages/coding/missing-number.tsx
@@ -18,6 +18,10 @@ const missingNumberCode = `/**
 // [3,0,1] -> 2
 // [0,1] -> 2
 // [9,6,4,2,3,5,7,0,1] -> 8
+// Approach 3:
+// Similar concept to approach 2 but can sum up [0..n] and also the nums array and subtract the 2 sums to get the missing number
+// Approach 4:
+// Can use XORs against the nums array and the numbers from [0..n]; the final result will be the missing number
 var missingNumber = function(nums) {
  // Compute the expected sum of the nums in advance if all the numbers were there
  const sum = (nums.length * (nums.length + 1)) / 2;
@@ -32,6 +36,28 @@ var missingNumber = function(nums) {
  return missingNumber;
 };`;
 
+const missingNumberXorCode = `// XOR Approach:
+// Can use XORs against the nums array (xor1) and the numbers from [0..n] (xor2); xor1 ^ xor2 will be the missing number
+var missingNumber = function(nums) {
+  // Handle empty case
+  if (nums.length === 0) {
+    return 0;
+  }
+  
+  // Xor all the original nums array into xor1
+  const xor1 = nums.reduce((acc, current) => { return acc ^ current; }, 0);
+  
+  // Xor all the numbers from [0..n] into xor2
+  let xor2 = 0;
+  for (let i = 0; i < nums.length + 1; i++) {
+    xor2 ^= i;
+  }
+  
+  // Xor the 2 results to get the missing number
+  const missingNumber = xor1 ^ xor2;
+  return missingNumber;
+};`;
+
 const MissingNumber: NextPage = () => {
   return (
     <div>
@@ -40,7 +66,14 @@ const MissingNumber: NextPage = () => {
         Given an array nums containing n distinct numbers in the range [0, n],
         return the only number in the range that is missing from the array.
       </p>
-      <Prism language="javascript">{missingNumberCode}</Prism>
+      <Prism.Tabs>
+        <Prism.Tab language="javascript" label="missingNumberSumApproach.js">
+          {missingNumberCode}
+        </Prism.Tab>
+        <Prism.Tab language="javascript" label="missingNumberXorApproach.js">
+          {missingNumberXorCode}
+        </Prism.Tab>
+      </Prism.Tabs>
     </div>
   );
 };
