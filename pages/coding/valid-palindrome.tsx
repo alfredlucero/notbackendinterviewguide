4a640b666cd1fd63902d5403ec7bbfffa2156e16
Alfred Lucero <alfred.lucero@ucla.edu>
2022-10-09 20:41:20 -0700

    Added cpp + java examples for valid palindrome problem

--- pages/coding/valid-palindrome.tsx
+++ pages/coding/valid-palindrome.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { Prism } from "@mantine/prism";
 
-const twoPointersCode = `/**
+const twoPointersJsCode = `/**
 * @param {string} s
 * @return {boolean}
 */
@@ -41,9 +41,79 @@ var isPalindrome = function(s) {
  return true;
 };`;
 
+const twoPointersCppCode = `class Solution {
+  public:
+      // O(N) time, O(1) space
+      bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        // Two pointer approach to check characters match on left and right and work your way inward
+        while (left < right) {
+          // Skip non-alphanumeric characters on the left
+          while (left < s.length() && !isalnum(s[left])) {
+            left++;
+          }
+          
+          // Skip non-alphanumeric characters on the right
+          while (right >= 0 && !isalnum(s[right])) {
+            right--;
+          }
+          
+          // Both left and right should be alphanumeric...
+          // If the characters don't match, we don't have a palindrome
+          if (left < right && tolower(s[left]) != tolower(s[right])) {
+            return false;
+          }
+          
+          left++;
+          right--;
+        }
+        
+        // We've checked all the characters, so it's a valid palindrome
+        return true;
+      }
+  };`;
+
+const twoPointersJavaCode = `class Solution {
+  // O(N) time, O(1) space
+  public boolean isPalindrome(String s) {
+      int left = 0;
+      int right = s.length() - 1;
+      
+      // Two pointer approach to check characters on left match characters on right and work your way inward
+      while (left < right) {
+        
+        // Keep on skipping the non-alphanumeric characters on the left
+        while(left < s.length() && !Character.isLetterOrDigit(s.charAt(left))) {
+          left++;
+        }
+        
+        // Keep on skipping the non-alphanumeric characters on the right
+        while (right >= 0 && !Character.isLetterOrDigit(s.charAt(right))) {
+          right--;
+        }
+        
+        // Both left and right should have alphanumeric characters if the indices haven't crossed...
+        // If both left and right characters do not match, we will return false since it can't be a palindrome
+        if (left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+          return false;
+        }
+        
+        left++;
+        right--;
+      }
+      
+      
+      // We've verified all alphanumeric characters form a palindrome
+      return true;
+  }
+}`;
+
 const ValidPalindrome: NextPage = () => {
   return (
     <div>
+      {/* NC150 */}
       <p>Source: https://leetcode.com/problems/valid-palindrome/</p>
       <p>
         A phrase is a palindrome if, after converting all uppercase letters into
@@ -53,8 +123,14 @@ const ValidPalindrome: NextPage = () => {
         false otherwise.
       </p>
       <Prism.Tabs>
-        <Prism.Tab label="stack.js" language="javascript">
-          {twoPointersCode}
+        <Prism.Tab label="twoPointers.js" language="javascript">
+          {twoPointersJsCode}
+        </Prism.Tab>
+        <Prism.Tab label="twoPointers.cpp" language="cpp">
+          {twoPointersCppCode}
+        </Prism.Tab>
+        <Prism.Tab label="twoPointers.java" language="diff">
+          {twoPointersJavaCode}
         </Prism.Tab>
       </Prism.Tabs>
     </div>
